5a58122cf0aa02cd213d124600a9fb89f1d2b5d9
Soham2402 <sohamhegdessj4@gmail.com>
2024-01-18 11:09:30 +0530

    Debugged

--- database.py
+++ database.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DB_URL = 'postgres://soham:uZWbPEXO9zghLvtSxTJlxyzYGhfvKLZh@dpg-cmkapbf109ks739kki80-a/matrixdb'
+# DB_URL = 'postgresql://postgres:postgres@localhost:5432/MatrixDb' 
+
+#You can use the below url if you dont want to setup pgadmin
+DB_URL = 'postgresql://soham:uZWbPEXO9zghLvtSxTJlxyzYGhfvKLZh@dpg-cmkapbf109ks739kki80-a.singapore-postgres.render.com/matrixdb'
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 Base = declarative_base()
