2b8a56f29f93a899d08fc8a486a051c4fc389134
Soham2402 <sohamhegdessj4@gmail.com>
2024-01-18 10:17:53 +0530

    [Refactored: Corrected the name of requirements.txt file]

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DB_URL = 'postgresql://postgres:postgres@localhost:5432/MatrixDb'
+DB_URL = 'postgres://soham:uZWbPEXO9zghLvtSxTJlxyzYGhfvKLZh@dpg-cmkapbf109ks739kki80-a/matrixdb'
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 Base = declarative_base()
+
